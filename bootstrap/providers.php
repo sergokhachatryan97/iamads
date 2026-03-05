2e3a977c92a9a02bbc30faae98a56d59d714c87b
Sergo Khachatryan <sergokhachatryan97@gmail.com>
2026-03-05 13:28:38 +0400

    fix

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\PaymentServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     SocialiteProviders\Manager\ServiceProvider::class,
 ];
