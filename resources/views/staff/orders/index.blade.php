e8a67553efc863e7a0abab6449300fc3cff555c2
Sergo Khachatryan <sergokhachatryan97@gmail.com>
2026-03-25 16:52:36 +0400

    fix

--- resources/views/staff/orders/index.blade.php
+++ resources/views/staff/orders/index.blade.php
@@ -426,7 +426,7 @@
                                             </a>
                                         </td>
                                         <td class="px-6 py-4">
-                                            <div class="flex items-center gap-3 min-w-[240px]">
+                                            <div class="flex items-center gap-3 min-w-[240px] max-w-[270px]">
                                                 @php
                                                     $delivered = (int) ($order->delivered ?? 0);
                                                     $quantity = (int) ($order->quantity ?? 0);
