6e5d08c29aeb550b6b91db99c276a3e013349ef2
Sergo Khachatryan <sergokhachatryan97@gmail.com>
2026-04-15 18:23:57 +0400

    fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -55,14 +55,14 @@
                             {{ __('Services') }}
                         </x-nav-link>
                     @endif
-            @if(Auth::guard('staff')->check())
-                <x-nav-link :href="route('staff.subscriptions.index')" :active="request()->routeIs('staff.subscriptions.index') || request()->routeIs('staff.subscriptions.create') || request()->routeIs('staff.subscriptions.edit') || request()->routeIs('staff.subscriptions.edit-header')">
-                    {{ __('Subscription Plans') }}
-                </x-nav-link>
-                <x-nav-link :href="route('staff.subscriptions.client-subscriptions')" :active="request()->routeIs('staff.subscriptions.client-subscriptions')">
-                    {{ __('Client Subscriptions') }}
-                </x-nav-link>
-            @endif
+{{--            @if(Auth::guard('staff')->check())--}}
+{{--                <x-nav-link :href="route('staff.subscriptions.index')" :active="request()->routeIs('staff.subscriptions.index') || request()->routeIs('staff.subscriptions.create') || request()->routeIs('staff.subscriptions.edit') || request()->routeIs('staff.subscriptions.edit-header')">--}}
+{{--                    {{ __('Subscription Plans') }}--}}
+{{--                </x-nav-link>--}}
+{{--                <x-nav-link :href="route('staff.subscriptions.client-subscriptions')" :active="request()->routeIs('staff.subscriptions.client-subscriptions')">--}}
+{{--                    {{ __('Client Subscriptions') }}--}}
+{{--                </x-nav-link>--}}
+{{--            @endif--}}
                 </div>
             </div>
 
@@ -171,14 +171,14 @@
                 </x-responsive-nav-link>
             @endif
 
-            @if(Auth::guard('staff')->check())
-                <x-responsive-nav-link :href="route('staff.subscriptions.index')" :active="request()->routeIs('staff.subscriptions.index') || request()->routeIs('staff.subscriptions.create') || request()->routeIs('staff.subscriptions.edit') || request()->routeIs('staff.subscriptions.edit-header')">
-                    {{ __('Subscription Plans') }}
-                </x-responsive-nav-link>
-                <x-responsive-nav-link :href="route('staff.subscriptions.client-subscriptions')" :active="request()->routeIs('staff.subscriptions.client-subscriptions')">
-                    {{ __('Client Subscriptions') }}
-                </x-responsive-nav-link>
-            @endif
+{{--            @if(Auth::guard('staff')->check())--}}
+{{--                <x-responsive-nav-link :href="route('staff.subscriptions.index')" :active="request()->routeIs('staff.subscriptions.index') || request()->routeIs('staff.subscriptions.create') || request()->routeIs('staff.subscriptions.edit') || request()->routeIs('staff.subscriptions.edit-header')">--}}
+{{--                    {{ __('Subscription Plans') }}--}}
+{{--                </x-responsive-nav-link>--}}
+{{--                <x-responsive-nav-link :href="route('staff.subscriptions.client-subscriptions')" :active="request()->routeIs('staff.subscriptions.client-subscriptions')">--}}
+{{--                    {{ __('Client Subscriptions') }}--}}
+{{--                </x-responsive-nav-link>--}}
+{{--            @endif--}}
         </div>
 
         <!-- Responsive Settings Options -->
