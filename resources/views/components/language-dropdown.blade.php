d562645df420f11c6d9b79655c90c5585fc3a5f8
Sergo Khachatryan <sergokhachatryan97@gmail.com>
2026-04-21 11:52:02 +0400

    fix

--- resources/views/components/language-dropdown.blade.php
+++ resources/views/components/language-dropdown.blade.php
@@ -93,15 +93,6 @@
 </select>
 @else
 <div class="relative" x-data="{ open: false }" @click.outside="open = false">
-    <button type="button"
-            @click="open = ! open"
-            class="inline-flex items-center gap-1.5 px-3 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-200 rounded-md hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-1 transition-colors">
-        <span class="text-lg">{{ $locales[$currentLocale]['flag'] ?? '🌐' }}</span>
-        <span>{{ $locales[$currentLocale]['native'] ?? strtoupper($currentLocale) }}</span>
-        <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-        </svg>
-    </button>
     <div x-show="open"
          x-transition:enter="transition ease-out duration-100"
          x-transition:enter-start="opacity-0 scale-95"
